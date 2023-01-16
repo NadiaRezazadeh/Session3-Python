79d80a708d9265aa6d777c145a0b668cfd7a5e64
NadiaRezazadeh <122272736+NadiaRezazadeh@users.noreply.github.com>
2023-01-16 15:51:28 +0330

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -3,8 +3,9 @@ import random
 pc_number = random.randint(0,100)
 t = 0
 
+n = print("Guess a number between 0 and 100") 
 while True:
-    user_number = int(input("Guess a number between 0 and 100 and enter it="))
+    user_number = int(input(" enter the number you guessed ="))
    
     t += 1
     if pc_number == user_number :
